58a7124382c51507df8da032aa8621bd2bdbb665
Felix Roske <felix@zalando.de>
2024-05-02 12:52:02 +0200

    Rename folder

---
+++ unsorted-kotlin/src/main/kotlin/centuryfromyear/Main.kt
@@ -0,0 +1,19 @@
+package centuryfromyear
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+fun century(number: Int): Int {
+    return number
+}
+
+class CenturyFromYearTest {
+
+    @Test fun testFixed() {
+        assertEquals(18, century(1705));
+        assertEquals(19, century(1900));
+        assertEquals(17, century(1601));
+        assertEquals(20, century(2000));
+        assertEquals(1,  century(89));
+    }
+}
--- unsorted-kotlin/src/main/kotlin/peopleinthebus/Main.kt
+++ unsorted-kotlin/src/main/kotlin/peopleinthebus/Main.kt
@@ -4,11 +4,18 @@ import kotlin.test.assertEquals
 import kotlin.test.Test
 
 fun people(busStops: Array<Pair<Int, Int>>) : Int {
-    //code here
-    return 0
+    var persons = 0
+    busStops.forEach {
+        persons = persons + it.first - it.second
+    }
+    return persons
 }
 
-class TestExample {
+// Voted as best practice:
+// fun people(busStops: Array<Pair<Int, Int>>) = busStops.sumBy { it.first - it.second }
+
+
+class PeopleInTheBusTest {
     @Test
     fun `Basic Tests`() {
         assertEquals(17, people(arrayOf(3 to 0,9 to 1,4 to 10,12 to 2,6 to 1,7 to 10)))
